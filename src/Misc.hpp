1fc57853e0de2b6b0b70e27ab54872c3a9298785
te9c <roman.te9c@gmail.com>
2023-01-24 19:48:02 +0500

    Not implemented method realization

--- src/Misc.hpp
+++ src/Misc.hpp
@@ -6,7 +6,8 @@ class Misc{
     public:
         static bool IsValidLongNotation(std::string notation);
 
-        static bool IsValidShortNotation(std::string notation);
+        // Not implemented
+        static bool IsValidShortNotation(std::string notation) {return false;}
 
         static bool IsValidNotation(std::string notation) {
             return IsValidLongNotation(notation) || IsValidShortNotation(notation);}
