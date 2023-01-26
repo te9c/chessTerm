41d3dc4aaf9200bd070f07e30089ce947aa70649
te9c <roman.te9c@gmail.com>
2023-01-26 23:09:57 +0500

    Fix notation validating function

--- src/Misc.cpp
+++ src/Misc.cpp
@@ -7,7 +7,7 @@ bool Misc::IsValidLongNotation(std::string notation){
         return false;
     if ((notation[0] - 'a' < 0 || notation[0] - 'a' > 7) || (notation[2] - 'a' < 0 || notation[2] - 'a' > 7))
         return false;
-    if ((notation[1] < 48 || notation[1] > 56) || (notation[3] < 48 || notation[3] > 56))
+    if ((notation[1] < 49 || notation[1] > 56) || (notation[3] < 49 || notation[3] > 56))
         return false;
 
     return true;
