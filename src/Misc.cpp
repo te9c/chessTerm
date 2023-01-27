2c46cf10502972b7b8fe924924bb113c5d6b620a
te9c <roman.te9c@gmail.com>
2023-01-27 15:59:43 +0500

    Optimize notation validating function

--- src/Misc.cpp
+++ src/Misc.cpp
@@ -6,12 +6,13 @@
 
 bool Misc::IsValidLongNotation(std::string notation){
 
+    if (notation.size() != 4)
+        return false;
+
     // transform to lowercase
     std::transform(notation.begin(), notation.end(), notation.begin(),
     [](unsigned char c){ return std::tolower(c); });
 
-    if (notation.size() != 4)
-        return false;
     if ((notation[0] - 'a' < 0 || notation[0] - 'a' > 7) || (notation[2] - 'a' < 0 || notation[2] - 'a' > 7))
         return false;
     if ((notation[1] < 49 || notation[1] > 56) || (notation[3] < 49 || notation[3] > 56))
