e234ec1b1506f78dd0b56be7e3d0f45e3e4aae1f
te9c <roman.te9c@gmail.com>
2023-02-15 15:59:42 +0500

    Improve Chessboard's tests

--- tests/ChessboardTest.cpp
+++ tests/ChessboardTest.cpp
@@ -1,12 +1,32 @@
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include "Chessboard.hpp"
+#include <array>
 
-// this is garbage
-TEST(ChessboardConstructor,testBoardWithValidInput){
-    Chessboard b1;
-    Chessboard b2("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    char** expb1 = new char*[8];
-    char expb2[8][8] = {
+//Note that when you initialize array you have to mirror your board
+
+TEST(ChessboardConstructor,EmptyBoardInput){
+    Chessboard cb;
+
+    std::array<std::array<char,8>,8> expectedBoard = {{
+        {' ',' ',' ',' ',' ',' ',' ',' '},
+        {' ',' ',' ',' ',' ',' ',' ',' '},
+        {' ',' ',' ',' ',' ',' ',' ',' '},
+        {' ',' ',' ',' ',' ',' ',' ',' '},
+        {' ',' ',' ',' ',' ',' ',' ',' '},
+        {' ',' ',' ',' ',' ',' ',' ',' '},
+        {' ',' ',' ',' ',' ',' ',' ',' '},
+        {' ',' ',' ',' ',' ',' ',' ',' '},
+    }};
+
+    EXPECT_THAT(cb.GetBoard(), testing::ElementsAreArray(expectedBoard));
+}
+
+TEST(ChessboardConstructor,StartBoardInput){
+
+    Chessboard cb("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+    std::array<std::array<char,8>,8> expectedBoard = {{
         {'R','N','B','Q','K','B','N','R'},
         {'P','P','P','P','P','P','P','P'},
         {' ',' ',' ',' ',' ',' ',' ',' '},
@@ -15,25 +35,6 @@ TEST(ChessboardConstructor,testBoardWithValidInput){
         {' ',' ',' ',' ',' ',' ',' ',' '},
         {'p','p','p','p','p','p','p','p'},
         {'r','n','b','q','k','b','n','r'},
-    };
-
-    for (int i = 0; i < 8; i++) {
-        expb1[i] = new char[8];
-    }
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            expb1[i][j] = ' ';
-        }
-    }
-
-    for (int i = 0; i < 8; i++) {
-        for(int j = 0; j < 8; j++){
-            EXPECT_EQ(b1.GetBoard()[i][j], expb1[i][j]) << "Arrays differ at index [" << i << "]" << "[" << j << "]";
-        }
-    }
-    for (int i = 0; i < 8; i++) {
-        for(int j = 0; j < 8; j++){
-            EXPECT_EQ(b2.GetBoard()[i][j], expb2[i][j]) << "Arrays differ at index [" << i << "]" << "[" << j << "]";
-        }
-    }
+    }};
+    EXPECT_THAT(cb.GetBoard(), testing::ElementsAreArray(expectedBoard));
 }
