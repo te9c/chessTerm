db41be9d2c1d57f03ee60a6270787c5ac41b4e6a
te9c <roman.te9c@gmail.com>
2023-02-18 15:07:43 +0500

    Add tests for GetFEN method

--- tests/ChessboardTest.cpp
+++ tests/ChessboardTest.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+
 #include "Chessboard.hpp"
 #include <array>
 
@@ -37,4 +38,17 @@ TEST(ChessboardConstructor,StartBoardInput){
         {'r','n','b','q','k','b','n','r'},
     }};
     EXPECT_THAT(cb.GetBoard(), testing::ElementsAreArray(expectedBoard));
+}
+
+TEST(ChessboardGetFen,StaticInput){
+    Chessboard cb1("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    Chessboard cb2;
+    Chessboard cb3("r4r1k/1Rp3pp/1N1p4/n2Bpb2/3Pn3/2q4P/5PPN/R2Q2K1 w - - 0 30");
+    Chessboard cb4("1rb2k1r/R2nqpbp/3p1np1/1p1P4/2p1PP2/2N1B3/1PQNB1PP/5RK1 b - d6 12 18");
+
+
+    EXPECT_EQ(cb1.GetFEN(),"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    EXPECT_EQ(cb2.GetFEN(),"8/8/8/8/8/8/8/8 w - - 0 1");
+    EXPECT_EQ(cb3.GetFEN(),"r4r1k/1Rp3pp/1N1p4/n2Bpb2/3Pn3/2q4P/5PPN/R2Q2K1 w - - 0 30");
+    EXPECT_EQ(cb4.GetFEN(),"1rb2k1r/R2nqpbp/3p1np1/1p1P4/2p1PP2/2N1B3/1PQNB1PP/5RK1 b - d6 12 18");
 }
